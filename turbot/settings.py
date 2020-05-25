427f947c36e560c1d4646c6a0718c5ded336e710
Thomas Lupin <thomas.lupin@epita.fr>
2020-05-25 11:18:52 +0200

    hosts: add heroku host

--- turbot/settings.py
+++ turbot/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["slack.thetoto.fr", "slack-dev.thetoto.fr"]
+ALLOWED_HOSTS = ["slack.thetoto.fr", "slack-dev.thetoto.fr", "newaltair.herokuapp.com"]
 
 
 # Application definition
